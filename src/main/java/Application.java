46364ae4c2b7d82c786cc7e9616ece06591a97ff
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-07-24 19:00:28 +0300

    main.class

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import ua.com.executor.GenerateOOM;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        GenerateOOM generateOOM = new GenerateOOM();
+        generateOOM.start();
+    }
+}
+
